99011b1611c9c16396abf5db69835ee9390ea076
orri23 <23krissi@gmail.com>
2017-04-06 12:55:19 +0000

    Add files via upload

---
+++ application/view/home/index.php
@@ -0,0 +1,31 @@
+<?php
+
+//Headers
+$loginUrl = 'http://138.68.150.56/Verkefni6/Login';
+$signupUrl = 'http://138.68.150.56/Verkefni6/Signup';
+
+if (isset($_POST['login']))
+{
+    header('Location: '.$loginUrl);
+}
+
+if (isset($_POST['signup']))
+{
+    header('Location: '.$signupUrl);
+}
+//Headers end
+
+
+
+?>
+
+<div class="container index">
+    <div class="box">
+        <form name="options" method="post">
+            <h3> Velkominn </h3> 
+            <h3> Vinsamlegast skráðu þig inn á núverandi aðgang eða búðu til nýjan aðgang </h3>
+            <input name="login" type="submit" value="Login" class="pure-button pure-button-active">
+            <input name="signup" type="submit" value="Sign Up" class="pure-button pure-button-active">
+        </form>
+    </div>
+</div>

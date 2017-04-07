bd5f9aadaf6758ce343356f1f0bbdd41e079f998
orri23 <23krissi@gmail.com>
2017-04-07 14:05:48 +0000

    Add files via upload

---
+++ application/view/loginsignup/login.php
@@ -0,0 +1,20 @@
+<?php
+
+
+?>
+
+<div class="container">
+    <div class="box">
+        <H1>Innskráning</H1>
+        <form name="login" method="post">
+
+            <label><b>Notendanafn:</b></label>
+            <input type="text" placeholder="" name="username" required><br>
+
+            <label><b>Lykilorð:</b></label>
+            <input type="password" placeholder="" name="Password" required><br>
+
+            <input name="login" type="submit" value="Skrá mig inn!" class="pure-button pure-button-active"><br>
+        </form>
+    </div>
+</div>

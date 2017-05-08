cdc3b1659af9b3d536500220be39ff5ceb7e563b
orri23 <23krissi@gmail.com>
2017-05-08 23:50:02 +0000

    Add files via upload

---
+++ application/view/home/privacy.php
@@ -0,0 +1,91 @@
+<?php
+
+
+
+?>
+
+<?php
+
+
+
+?>
+
+
+ 
+   
+
+<div class="splash-container">
+    <div class="splash">
+        <h1 class="splash-head">Privacy Policy </h1>
+       
+         <h4 class="is-center"> We are keeping your password safe via hash. </h4>
+         <h4 class="is-center"> ........................................... </h4>
+         <h4 class="is-center"> ........................................... </h4>
+         <h4 class="is-center"> ........................................... </h4>
+
+
+
+        
+    </div>
+</div>
+
+
+        
+
+
+
+        </div>
+    </div>
+</div>
+</head>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    
+    <style>
+    
+
+
+.splash-head {
+  display: inline-block;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  padding: 10px;
+  font: normal 48px/normal "Warnes", Helvetica, sans-serif;
+  color: rgba(255,255,255,1);
+  text-decoration: normal;
+  text-align: center;
+  -o-text-overflow: clip;
+  text-overflow: clip;
+  white-space: pre;
+  text-shadow: 0 0 10px rgba(255,255,255,1) , 0 0 20px rgba(255,255,255,1) , 0 0 30px rgba(255,255,255,1) , 0 0 40px #00994d , 0 0 70px #00994d , 0 0 80px #00994d , 0 0 100px #00994d ;
+  -webkit-transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1);
+  -moz-transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1);
+  -o-transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1);
+  transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1);
+  border: none;
+  text-transform: none;
+}
+
+.splash-head:hover {
+  text-shadow: 0 0 10px rgba(255,255,255,1) , 0 0 20px rgba(255,255,255,1) , 0 0 30px rgba(255,255,255,1) , 0 0 40px #660080 , 0 0 70px #660080 , 0 0 80px #660080 , 0 0 100px #660080 ;
+}
+
+h4 {
+    text-transform: none;
+    font-size: 14.5px;
+}
+   
+     
+    </style>
+
+     
+   
+     
+    
+</head>
+</html>
+
+
